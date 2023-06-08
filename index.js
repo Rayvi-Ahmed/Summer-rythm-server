5596e900a88d9614f314ca5d426c06d207d4652f
Misbah Uddin <rayvi73@gmail.com>
2023-06-08 15:03:37 +0600

    login restructed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -12,9 +14,7 @@ app.get('/', (req, res) => {
     res.send('Tunig on the rythme')
 })
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.poxvpxp.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.poxvpxp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +29,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const studentCollection = client.db('MusicClass').collection('student')
+
+        app.post('/student', async (req, res) => {
+            const student = req.body
+            const query = { email: student.email }
+            const existingStudent = await studentCollection.findOne(query)
+            if (existingStudent) {
+                return res.send({ massage: 'This student already have account' })
+            }
+            const result = await studentCollection.insertOne(student)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -38,9 +52,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
-
 
 
 
